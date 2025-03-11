151f6cb0b96e5ca22c0e3e66e3e5cefed37ba7ef
Sameerraza7760 <sameerraza7760@gmail.com>
2025-03-11 09:33:34 -0700

    update

--- validations/post.Validation.js
+++ validations/post.Validation.js
@@ -14,3 +14,12 @@ export const postDetail = vine.object({
     imageUrl: vine.string().url().optional(),
     userId: vine.string().uuid().required(), // Assuming UUID for user reference
 });
+
+
+export const postDetailf= vine.object({
+    title: vine.string().minLength(3).maxLength(100).trim().required(),
+    content: vine.string().minLength(10).required(),
+    imageUrl: vine.string().url().optional(),
+    userId: vine.string().uuid().required(), // Assuming UUID for user reference
+});
+
