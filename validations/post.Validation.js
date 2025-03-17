e0ffad6d911afa7c05f30ab68bb6a36d863cac64
Sameerraza7760 <sameerraza7760@gmail.com>
2025-03-17 11:06:37 -0700

    update from zohan

--- validations/post.Validation.js
+++ validations/post.Validation.js
@@ -49,3 +49,12 @@ export const postDetailffssss= vine.object({
 
 
 
+
+export const postDetailffssgss= vine.object({
+    title: vine.string().minLength(3).maxLength(100).trim().required(),
+    content: vine.string().minLength(10).required(),
+    imageUrl: vine.string().url().optional(),
+    userId: vine.string().uuid().required(), // Assuming UUID for user reference
+});
+
+
